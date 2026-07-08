a4f99632dc6d248e460f953573471a2c4e3d8ff1
janilsonmathias-alt <janilson.mathias@gmail.com>
2026-07-08 20:34:11 -0300

    Create controlid_api.py

---
+++ controlid_api.py
@@ -0,0 +1,45 @@
+import requests
+
+CONTROLID_IP = "192.168.0.100"
+CONTROLID_PORTA = 80
+
+URL = f"http://{{CONTROLID_IP}}:{{CONTROLID_PORTA}}"
+
+def liberar_usuario(controlid_id):
+  dados = {
+    "users":[
+      { 
+        "id" : controlid_id,
+        "registration_enable" : TRUE
+      }
+    ]
+  }
+  
+  r = request.post(
+    URL + "/user_set.cgi",
+    json = dados,
+    timeout = 10
+  )
+
+  return r.status_code == 200
+
+
+def bloquear_usuario(controlid_id):
+  dados = {
+    "users":[
+      {
+        "id" : controlid_id,
+        "registration_enable" : FALSE
+      }
+    ]
+  }
+
+  r = request.post(
+    URL + "/user_set.cgi",
+    json = dados,
+    timeout = 10
+  )
+
+  return r.status_code == 200
+
+
